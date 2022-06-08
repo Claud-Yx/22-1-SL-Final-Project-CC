eb4b8e3a2833a03a5c8208f9cd70ebebf74d7b08
ClaudYx <80788446+Claud-Yx@users.noreply.github.com>
2022-06-08 15:56:01 +0900

    add dayofweek
    + dayofweek

--- date_button.py
+++ date_button.py
@@ -2,14 +2,18 @@ import tkinter.font
 from tkinter import *
 from tkinter.ttk import *
 
+DAY_OF_WEEK = ('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun')
 
 class DateButton(Frame):
     def __init__(self, master, date, month, running_count=None, deadline_count=None, data=dict, **kw):
         super().__init__(master, **kw)
         self.configure(relief=RAISED, border=1, padding=1)
 
-        self.month = month
-        self.data = data
+        self.dayofweek = None
+        self.month = IntVar(value=month)
+        self.date = IntVar(value=date)
+        self.running_count = IntVar(value=running_count)
+        self.deadline_count = IntVar(value=deadline_count)
 
         self.date_font = tkinter.font.Font(size=10, weight='bold')
 
@@ -18,16 +22,16 @@ class DateButton(Frame):
         self.date_frame.pack()
         self.info_frame.pack()
 
-        self.date = Label(self.date_frame, text=date, width=7, font=self.date_font)
-        self.running_info = Label(self.info_frame, text=running_count, width=7, anchor=E, foreground='green')
-        self.deadline_info = Label(self.info_frame, text=deadline_count, width=7, anchor=E, foreground='red')
+        self.date_label = Label(self.date_frame, textvariable=self.date, width=7, font=self.date_font)
+        self.running_info = Label(self.info_frame, textvariable=self.deadline_count, width=7, anchor=E, foreground='green')
+        self.deadline_info = Label(self.info_frame, textvariable=self.deadline_count, width=7, anchor=E, foreground='red')
 
-        self.date.pack()
+        self.date_label.pack()
         self.running_info.pack()
         self.deadline_info.pack()
 
-        self.date.bind('<Button-1>', self.pressed)
-        self.date.bind('<ButtonRelease-1>', self.released)
+        self.date_label.bind('<Button-1>', self.pressed)
+        self.date_label.bind('<ButtonRelease-1>', self.released)
         self.running_info.bind('<Button-1>', self.pressed)
         self.running_info.bind('<ButtonRelease-1>', self.released)
         self.deadline_info.bind('<Button-1>', self.pressed)
@@ -40,19 +44,19 @@ class DateButton(Frame):
         self.configure(relief=RAISED)
 
     def date_color(self, color):
-        self.date.configure(foreground=color)
+        self.date_label.configure(foreground=color)
 
     def disable(self):
         self.configure(relief=GROOVE)
-        self.date.configure(foreground='gray')
+        self.date_label.configure(foreground='gray')
         self.running_info.configure(text="")
         self.deadline_info.configure(text="")
 
         def f(event=None):
             pass
 
-        self.date.bind("<Button-1>", f)
-        self.date.bind("<ButtonRelease-1>", f)
+        self.date_label.bind("<Button-1>", f)
+        self.date_label.bind("<ButtonRelease-1>", f)
         self.running_info.bind("<Button-1>", f)
         self.running_info.bind("<ButtonRelease-1>", f)
         self.deadline_info.bind("<Button-1>", f)
@@ -63,9 +67,13 @@ class DateButton(Frame):
             self.configure(relief=RAISED)
             fn(event)
 
-        self.date.bind("<ButtonRelease-1>", new_release)
+        self.date_label.bind("<ButtonRelease-1>", new_release)
         self.running_info.bind("<ButtonRelease-1>", new_release)
         self.deadline_info.bind("<ButtonRelease-1>", new_release)
+
+    def set_dayofweek(self, day):
+        assert day in DAY_OF_WEEK, print(f"DAY OF WEEK error: {day}")
+        self.dayofweek = day
 
 
 if __name__ == '__main__':
