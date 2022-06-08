cdf8bca22b804357dd17a3274d4dc4536f5ad69c
ClaudYx <80788446+Claud-Yx@users.noreply.github.com>
2022-06-08 17:14:00 +0900

    뭔가 여러가지 추가함

--- date_button.py
+++ date_button.py
@@ -2,14 +2,19 @@ import tkinter.font
 from tkinter import *
 from tkinter.ttk import *
 
+from requests import delete
+
 DAY_OF_WEEK = ('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun')
 
 class DateButton(Frame):
-    def __init__(self, master, date, month, running_count=None, deadline_count=None, data=dict, **kw):
+    def __init__(self, master, date=None, month=None, dayofweek=None, running_count=None, deadline_count=None, data=dict, **kw):
         super().__init__(master, **kw)
         self.configure(relief=RAISED, border=1, padding=1)
 
-        self.dayofweek = None
+        self.dayofweek = StringVar()
+        self.new_fn = self.released
+
+        self.delete_text = StringVar(value="")
         self.month = IntVar(value=month)
         self.date = IntVar(value=date)
         self.running_count = IntVar(value=running_count)
@@ -22,9 +27,12 @@ class DateButton(Frame):
         self.date_frame.pack()
         self.info_frame.pack()
 
-        self.date_label = Label(self.date_frame, textvariable=self.date, width=7, font=self.date_font)
-        self.running_info = Label(self.info_frame, textvariable=self.deadline_count, width=7, anchor=E, foreground='green')
-        self.deadline_info = Label(self.info_frame, textvariable=self.deadline_count, width=7, anchor=E, foreground='red')
+        self.date_label = Label(self.date_frame, textvariable=self.date, width=9, font=self.date_font)
+        self.running_info = Label(self.info_frame, textvariable=self.deadline_count, width=9, anchor=E, foreground='green')
+        self.deadline_info = Label(self.info_frame, textvariable=self.deadline_count, width=9, anchor=E, foreground='red')
+
+        if dayofweek:
+            self.set_dayofweek(dayofweek)
 
         self.date_label.pack()
         self.running_info.pack()
@@ -49,8 +57,8 @@ class DateButton(Frame):
     def disable(self):
         self.configure(relief=GROOVE)
         self.date_label.configure(foreground='gray')
-        self.running_info.configure(text="")
-        self.deadline_info.configure(text="")
+        self.running_info.configure(textvariable=self.delete_text)
+        self.deadline_info.configure(textvariable=self.delete_text)
 
         def f(event=None):
             pass
@@ -62,10 +70,32 @@ class DateButton(Frame):
         self.deadline_info.bind("<Button-1>", f)
         self.deadline_info.bind("<ButtonRelease-1>", f)
 
+    def enable(self):
+        self.configure(relief=RAISED)
+
+        if self.dayofweek.get() == 'sun':
+            self.date_label.configure(foreground='red')
+        elif self.dayofweek.get() == 'sat':
+            self.date_label.configure(foreground='blue')
+        else:
+            self.date_label.configure(foreground='black')
+
+        self.running_info.configure(textvariable=self.running_count)
+        self.deadline_info.configure(textvariable=self.deadline_count)
+
+        self.date_label.bind("<Button-1>", self.pressed)
+        self.date_label.bind("<ButtonRelease-1>", self.new_fn)
+        self.running_info.bind("<Button-1>", self.pressed)
+        self.running_info.bind("<ButtonRelease-1>", self.new_fn)
+        self.deadline_info.bind("<Button-1>", self.pressed)
+        self.deadline_info.bind("<ButtonRelease-1>", self.new_fn)
+
     def release_bind(self, fn):
         def new_release(event=None):
             self.configure(relief=RAISED)
             fn(event)
+
+        self.new_fn = new_release
 
         self.date_label.bind("<ButtonRelease-1>", new_release)
         self.running_info.bind("<ButtonRelease-1>", new_release)
@@ -73,7 +103,14 @@ class DateButton(Frame):
 
     def set_dayofweek(self, day):
         assert day in DAY_OF_WEEK, print(f"DAY OF WEEK error: {day}")
-        self.dayofweek = day
+        self.dayofweek.set(day)
+
+        if day == 'sun':
+            self.date_label.configure(foreground='red')
+        elif day == 'sat':
+            self.date_label.configure(foreground='blue')
+        else:
+            self.date_label.configure(foreground='black')
 
 
 if __name__ == '__main__':
@@ -89,19 +126,26 @@ if __name__ == '__main__':
     line1.pack(padx=5)
     line2.pack(padx=5)
 
-    date1 = DateButton(line1, 1, 6, running_count=5)
-    date2 = DateButton(line1, 2, 6, running_count=152)
-    date3 = DateButton(line2, 3, 6, running_count=12, deadline_count=2)
-    date4 = DateButton(line2, 4, 6)
+    date1 = DateButton(line1, 1, 6, 'sun', running_count=5)
+    date2 = DateButton(line1, 2, 6, 'sat', running_count=152)
+    date3 = DateButton(line2, 3, 6, 'sat', running_count=12, deadline_count=2)
+    date4 = DateButton(line2, 4, 6, 'mon')
 
-    date1.date_color('red')
-    date3.date_color('blue')
+    def disable_date2(event=None):
+        date2.disable()
+
+    date2.release_bind(disable_date2)
 
     def print_test(event=None):
         print("binding test")
 
     date3.release_bind(print_test)
     date2.disable()
+
+    def enable_date2(event=None):
+        date2.enable()
+
+    date4.release_bind(enable_date2)
 
     date1.pack(side=LEFT)
     date2.pack(side=LEFT)
